f44591b101eea85f3f73ad40ef175b76bc631cbf
brondo138 <93743624+brondo138@users.noreply.github.com>
2024-03-14 22:17:09 -0600

    Update Dispositivo.java

--- Foro2/src/main/java/com/mycompany/registrar/Dispositivo.java
+++ Foro2/src/main/java/com/mycompany/registrar/Dispositivo.java
@@ -1,6 +1,5 @@
 package com.mycompany.registrar;
 
-import com.mycompany.main.Main;
 import com.mycompany.mostrar.Mostrar;
 import javax.swing.JOptionPane;
 import java.util.ArrayList;
@@ -21,6 +20,14 @@ public class Dispositivo {
         this.microprocesador = microprocesador;
         this.memoriaRam = memoriaRam;
         this.tarjetaGrafica = tarjetaGrafica;
+        this.tamañoTorre = tamañoTorre;
+        this.discoDuro = discoDuro;
+    }
+    public Dispositivo(String fabricante, String modelo, String microprocesador, String memoriaRam, String tamañoTorre, String discoDuro) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.microprocesador = microprocesador;
+        this.memoriaRam = memoriaRam;
         this.tamañoTorre = tamañoTorre;
         this.discoDuro = discoDuro;
     }
@@ -52,28 +59,68 @@ public class Dispositivo {
     public static String getDiscoDuro() {
         return discoDuro;
     }
-
-     public static ArrayList ingresoDatos(ArrayList<String> listaDesktops) {
+    
+    /*public static void  ingresoDatos(ArrayList<String> listaDesktops) {
         String[] opciones = {"Desktops", "Laptops", "Tablets"};
 
-        boolean continuar = true;
-
-            int opcionSeleccionada = JOptionPane.showOptionDialog(null, "Qué tipo de dispositivo va a agregar:", "Registrar equipo", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
-            switch (opcionSeleccionada) {
-                case 0:
-                    Desktops desktops = new Desktops();
-                    listaDesktops.addAll(Desktops.desktopList(Desktops.getFabricante(), Desktops.getModelo(), Desktops.getMicroprocesador(), Desktops.getMemoriaRam(), Desktops.getTarjetaGrafica(), Desktops.getTamañoTorre(), Desktops.getDiscoDuro()));   
+        int opcionSeleccionada = JOptionPane.showOptionDialog(null, "Qué tipo de dispositivo va a agregar:", "Registrar equipo", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+        switch (opcionSeleccionada) {
+            case 0:
+                // Llamamos a la función desktops de la clase Desktops
+                Desktops desktops = new Desktops();
+                // Aloja las variables en la lista
+                listaDesktops.addAll(Desktops.desktopList(Desktops.getFabricante(), Desktops.getModelo(), Desktops.getMicroprocesador(), Desktops.getMemoriaRam(), Desktops.getTarjetaGrafica(), Desktops.getTamañoTorre(), Desktops.getDiscoDuro()));
                 break;
 
-                case 1:
-                    
-                case 2:
-                    // Lógica para Laptops
-                    Mostrar.mostrarLista(listaDesktops);
-                    break;
-            }
-       
+            case 1:
+                Laptops laptops = new Laptops();
+                listaDesktops.addAll(Laptops.laptopsList(Laptops.getFabricante(), Laptops.getModelo(), Laptops.getMicroprocesador(), Laptops.getMemoriaRam(), Laptops.getTamañoTorre(), Laptops.getDiscoDuro()));
+                break;
+                
 
-        return listaDesktops;
+            case 2:
+                // Lógica para Tablets
+                break;
+        }
+        
+
+    }*/
+    
+    public static int tipoDispositivo(){
+        String[] opciones = {"Desktops", "Laptops", "Tablets"};
+
+        return JOptionPane.showOptionDialog(null, "Qué tipo de dispositivo va a agregar:", "Registrar equipo", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
     }
+    private static ArrayList<String> finalD = new ArrayList<>();
+    private static ArrayList<String> finalL = new ArrayList<>();
+    public static void ingresoDatos(int tipo){
+        if (tipo == 0){
+            
+            
+            // Llamamos a la función desktops de la clase Desktops
+            Desktops desktops = new Desktops();
+            // Aloja las variables en la lista
+            finalD = desktops.desktopList(fabricante, modelo, microprocesador, memoriaRam, tarjetaGrafica, tamañoTorre, discoDuro);
+                
+        }
+        if (tipo == 1){
+
+            // Llamamos a la función desktops de la clase Desktops
+            Laptops laptops = new Laptops();
+            // Aloja las variables en la lista
+            finalL = laptops.laptopsList(fabricante, modelo, microprocesador, memoriaRam, tamañoTorre, discoDuro);
+            
+                
+        }
+    }
+    
+    public static ArrayList<String>[] obtenerDatos() {
+        ArrayList<String>[] datos = new ArrayList[2]; // Creamos un arreglo de dos ArrayLists
+        datos[0] = finalD; // Asignamos finalD al primer elemento del arreglo
+        datos[1] = finalL; // Asignamos finalL al segundo elemento del arreglo
+        return datos;
+    }
+    
+     
+    
 }

6e3739d319f9bddd6dc723eb705b06062fdb3a9c
NeXM2k4 <155313145+NeXM2k4@users.noreply.github.com>
2024-03-17 21:16:02 -0600

    Update Mostrar.java

--- Foro2/src/main/java/com/mycompany/mostrar/Mostrar.java
+++ Foro2/src/main/java/com/mycompany/mostrar/Mostrar.java
@@ -1,20 +1,19 @@
-package com.mycompany.mostrar;
+package Mostrar;
 
-import com.mycompany.registrar.Desktops;
-import com.mycompany.registrar.Dispositivo;
-import com.mycompany.registrar.Laptops;
+import Registrar.Dispositivo;
+
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class Mostrar {
-    
-public static void mostrarLista(ArrayList<String> lista) {
+
+    public static void mostrarLista(ArrayList<String> lista) {
         System.out.println("Lista de escritorios:");
         int totalEscritorios = lista.size() / 7; // Calculamos el número total de escritorios
-        
+
         for (int i = 0; i < totalEscritorios; i++) {
             int indice = i * 7; // Calculamos el índice inicial de cada escritorio
-            
+
             System.out.println("Fabricante: " + lista.get(indice));
             System.out.println("Modelo: " + lista.get(indice + 1));
             System.out.println("Microprocesador: " + lista.get(indice + 2));
@@ -26,8 +25,8 @@ public static void mostrarLista(ArrayList<String> lista) {
         }
     }
 
-public static void mostrarListaL(ArrayList<String> lista) {
-    System.out.println("Lista de laptops:");
+    public static void mostrarListaL(ArrayList<String> lista) {
+        System.out.println("Lista de laptops:");
         for (int i = 0; i < lista.size(); i += 6) {
             System.out.println("Fabricante: " + lista.get(i));
             System.out.println("Modelo: " + lista.get(i + 1));
@@ -37,31 +36,45 @@ public static void mostrarListaL(ArrayList<String> lista) {
             System.out.println("Capacidad de Disco Duro: " + lista.get(i + 5));
             System.out.println();
         }
-}
-    
+    }
+
+    public static void mostrarListaT(ArrayList<String> lista) {
+        System.out.println("Lista de laptops:");
+        for (int i = 0; i < lista.size(); i += 7) {
+            System.out.println("Fabricante: " + lista.get(i));
+            System.out.println("Modelo: " + lista.get(i + 1));
+            System.out.println("Microprocesador: " + lista.get(i + 2));
+            System.out.println("Tamaño Diagonal: " + lista.get(i + 3));
+            System.out.println("¿Capacitiva?/¿Resistiva?: " + lista.get(i + 4));
+            System.out.println("Memoria NAND: " + lista.get(i + 5));
+            System.out.println("Sistema Operativo: " + lista.get(i + 5));
+            System.out.println();
+        }
+    }
+
     public static void mostrar (ArrayList <String> lista){
-        
-        
+
+
         ArrayList<String>[] datos = Dispositivo.obtenerDatos();
-        
-        
 
-        String[] opciones = {"Desktops", "Laptops", "Tablets"};
 
-        int opcionSeleccionada = JOptionPane.showOptionDialog(null, "Qué tipo de dispositivo quiere ver:", "Mostrar equipo", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+
+        String[] opciones = {"Registrar.Desktops", "Registrar.Laptops", "Registrar.Tablets"};
+
+        int opcionSeleccionada = JOptionPane.showOptionDialog(null, "Qué tipo de dispositivo quiere ver:", "Mostrar.Mostrar equipo", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
         switch (opcionSeleccionada) {
             case 0:
-                 System.out.println("Elementos de finalD:");
-        for (String elemento : datos[0]) {
-            System.out.println(elemento);
-        }break;
+                System.out.println("Elementos de finalD:");
+                for (String elemento : datos[0]) {
+                    System.out.println(elemento);
+                }break;
             case 1:
-                 System.out.println("Elementos de finalL:");
+                System.out.println("Elementos de finalL:");
                 for (String elemento : datos[1]) {
                     System.out.println(elemento);
                 }
                 break;
-                
+
 
             case 2:
                 System.out.println("Elementos de finalT:");
@@ -70,10 +83,9 @@ public static void mostrarListaL(ArrayList<String> lista) {
                 }
                 break;
         }
-        
 
-    
-        
+
+
+
     }
 }
-
